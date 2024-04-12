cbcebd90a50545081b25873f85743b66b19bdbe1
blkcor <blkcor.dev@gmail.com>
2024-04-12 17:37:48 +0800

    chore: get database version endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,11 +1,29 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{
+    get,
+    web::{self, ServiceConfig},
+};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 
+struct AppState {
+    pool: sqlx::PgPool,
+}
+
 #[get("/")]
 async fn hello_world() -> &'static str {
     "Hello World!"
+}
+
+#[get("/version")]
+async fn version(app_state: web::Data<AppState>) -> String {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
+        .fetch_one(&app_state.pool)
+        .await;
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
 }
 
 #[shuttle_runtime::main]
@@ -20,9 +38,13 @@ async fn actix_web(
         .await
         .map_err(CustomError::new)?;
 
+    //initialize the app state
+    let app_state = web::Data::new(AppState { pool });
     // Configure the Actix Web service
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.app_data(app_state)
+            .service(hello_world)
+            .service(version);
     };
     Ok(config.into())
 }
