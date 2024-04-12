b61e9a51bcec602e4ef50982b269d6c769a79040
blkcor <blkcor.dev@gmail.com>
2024-04-12 17:16:57 +0800

    chore: setup database

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
+use sqlx::Executor;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -9,13 +11,18 @@ async fn hello_world() -> &'static str {
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres(
-        local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:5432/blkcor"
+        local_uri = "postgres://chenzilong:{secrets.PASSWORD}@localhost:5432/postgres"
     )]
     pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    // Create the database schema
+    pool.execute(include_str!("../../db/schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    // Configure the Actix Web service
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
     };
-
     Ok(config.into())
 }
