fe5741b9d6989e22e0f5ab4fdd5a38312b6e8ea6
blkcor <blkcor.dev@gmail.com>
2024-04-13 22:23:28 +0800

    chore: add postgre film repository

--- api/lib/src/film_repository/mod.rs
+++ api/lib/src/film_repository/mod.rs
@@ -4,11 +4,14 @@ use sqlx::types::Uuid;
 pub type FilmError = String;
 pub type FilmResult<T> = Result<T, FilmError>;
 
+pub mod postgres_film_repository;
+pub use postgres_film_repository::PostgresFilmRepository;
+
 #[async_trait]
 pub trait FilmRepository: Send + Sync + 'static {
     async fn get_films(&self) -> FilmResult<Vec<Film>>;
     async fn get_film(&self, id: &Uuid) -> FilmResult<Film>;
     async fn create_film(&self, id: &CreateFilm) -> FilmResult<Film>;
-    async fn update_film(&self, id: &Uuid) -> FilmResult<Uuid>;
+    async fn update_film(&self, id: &Film) -> FilmResult<Film>;
     async fn delete_film(&self, id: &Uuid) -> FilmResult<Uuid>;
 }
