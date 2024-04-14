92de4f4abb4214f0787e02c29b36c8de4511baf3
blkcor <blkcor.dev@gmail.com>
2024-04-14 14:12:44 +0800

    chore: css watcher and add logger for front app

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,19 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    //initialize the logger
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
+    log::info!("hello console");
+    // Launch the web application using the App component as the root.
+    launch(App);
+}
+
+// Define a component that renders a div with the text "Hello, world!"
+fn App() -> Element {
+    rsx! {
+        div {
+            "Hello, DevBcn!"
+        }
+    }
 }
