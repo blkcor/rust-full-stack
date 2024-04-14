4ca1ece0a3aa3ed4c7ab239ddcf8451603ab97b9
blkcor <blkcor.dev@gmail.com>
2024-04-14 16:19:37 +0800

    chore: header and footer components

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case)]
-
+mod components;
+use components::{Footer, Header};
 use dioxus::prelude::*;
+
 fn main() {
     //initialize the logger
     wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
@@ -12,8 +14,13 @@ fn main() {
 // Define a component that renders a div with the text "Hello, world!"
 fn App() -> Element {
     rsx! {
-        div {
-            "Hello, DevBcn!"
+        main{
+            class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+            Header{},
+            section{
+                class: "md:container md:mx-auto md:py-8 flex-1"
+            },
+            Footer{}
         }
     }
 }
